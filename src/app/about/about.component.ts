5798ce562723e7bf9c3831f60c43449313f93217
martinbarreirob <martinbarreirob@gmail.com>
2024-01-19 18:19:04 +0100

    Arreglos responsive

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
   public images = ['/assets/about1.webp', '/assets/about2.webp', '/assets/about3.webp'];
+  public studies = [
+    {
+      title: 'CS Desarrollado de Aplicaciones Web',
+      center: 'IES San Clemente',
+      data: '2021 / 2023',
+      description: '<p> Durante el desarrollo de este Ciclo, adquirí sólidos fundamentos en <strong>programación orientada a objetos</strong> y me familiaricé con el uso de los frameworks Symfony y Vue.</p><p>En el ámbito del desarrollo backend, aproveché la oportunidad de aprender a crear contenedores Docker para implementar eficientemente mis proyectos.</p><p>Aunque esta etapa me proporcionó conocimientos fundamentales en diversas áreas, también marcó el comienzo para identificar mis verdaderos intereses.</p>',
+      technologies: ['Docker','Vue','PHP','Symfony','Javascript'],
+    },
+    {
+      title: 'CS Administración de Sistemas Informáticos en Red',
+      center: 'IES Rodeira',
+      data: '2019 / 2021',
+      description: '<p>Aquí principalmente aprendí sobre la gestión y <strong>despliegue de servidores</strong> utilizando Docker y Apache principalmente además de trabajar con Bases de Datos SQL.</p><p>Estos estudios también me sirvieron para descubrir la programación e hicieron solida mi decisión de seguir estudiando.</p>',
+      technologies: ['Apache','SQL','Linux','Docker','Python', 'PHP'],
+    },
+  ]
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,13 +8,28 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
 
+  public experiences = [{
+    empresa: 'Galaxia Empirica - pInvestiga',
+    puesto: 'Desarrollador Frontend',
+    data: 'Octubre 2023 / Diciembre 2023',
+    description: '<p>Durante mi período de prácticas en la empresa, colaboré activamente en el desarrollo de la nueva versión del software pInvestiga utilizando Angular y NestJS.</p><p>Mi responsabilidad principal consistía en la creación de diversos componentes y servicios para la aplicación. Entre las tareas destacadas se incluyeron la implementación de un <strong>sistema de chat en tiempo real</strong>, el desarrollo de bibliotecas de datos y la creación de formularios para el ingreso de datos, entre otras contribuciones significativas.</p>',
+    technologies: ['NodeJs', 'Angular', 'PHP', 'SQL', 'Github'],
+    recomendation: '/assets/cartarecomendacion.pdf',
+  }];
+
+  public proyects = [{
+    title: 'Tres En Raya Online',
+    subtitle: 'Proyecto CS DAW',
+    description: '<p> A pesar de ser un juego simple que todo el mundo conoce, decidí llevarlo al siguiente nivel al añadir soporte para jugar en línea con otros usuarios. Esta implementación permite a los jugadores crear o unirse a partidas en tiempo real, además de facilitar la comunicación a través de un chat de texto integrado.</p><p>La aplicación está desarrollada utilizando Angular en el lado del cliente y un backend en NestJS con una API Gateway y WebSockets para facilitar la comunicación en tiempo real. Este proyecto ha sido desarrollado de manera autodidacta al 100%, representando una valiosa experiencia formativa.</p>',
+    technologies: ['NodeJs', 'Angular', 'Socket.IO', 'Apache'],
+    github: 'https://github.com/martinbarreirob/tresOnline2',
+  }];
+
   constructor(private router: Router) { }
 
-  abrirPDF() {
-    const urlPDF = '/assets/cartarecomendacion.pdf';
-    this.router.navigateByUrl(urlPDF, { skipLocationChange: true });
-    window.open(urlPDF, '_blank');
+  abrirPDF(url: string) {
+    this.router.navigateByUrl(url, { skipLocationChange: true });
+    window.open(url, '_blank');
   }
-
 
 }
